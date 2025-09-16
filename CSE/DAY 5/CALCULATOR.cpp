971d87fbfb18dcce951fbf432c9a6769e95c3461
soumyadipdinda <soumyadipdindax@gmail.com>
2025-09-16 15:11:06 +0530

    Add files via upload

---
+++ CSE/DAY 5/CALCULATOR.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+  char op;
+  double first, second;
+  printf("Enter an operator (+, -, *, /): ");
+  scanf("%c", &op);
+  printf("Enter two operands: ");
+  scanf("%lf %lf", &first, &second);
+  switch (op)
+  {
+    case '+':
+      printf("%lf + %lf = %lf", first, second, first + second);
+      break;
+    case '-':
+      printf("%lf - %lf = %lf", first, second, first - second);
+      break;
+    case '*':
+      printf("%lf * %lf = %lf", first, second, first * second);
+      break;
+    case '/':
+      printf("%lf / %lf = %lf", first, second, first / second);
+      break;
+  }
+
+  return 0;
+}
---
+++ CSE/DAY 5/VOWEL CONSONENT.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+    main()
+{
+    char alpht;
+    printf("\nEnter an alphabet:- ");
+    scanf("%c",&alpht);
+    switch(alpht)
+   {
+      case 'a' :
+        printf("\nIt is a vowel");
+        break;
+      case 'e' :
+        printf("\nIt is a vowel");
+        break;
+      case 'i' :
+        printf("\nIt is a vowel");
+        break;
+      case 'o' :
+        printf("\nIt is a vowel");
+        break;
+      case 'u' :
+        printf("\nIt is a vowel");
+        break;
+      default :
+        printf("\nIt is not a vowel"); 
+	}
+    return 0;
+ }
