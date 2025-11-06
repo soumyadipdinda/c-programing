8387b96a9a26a31c13eae53dcafd8f5a5f6e90ae
soumyadipdinda <soumyadipdindax@gmail.com>
2025-11-07 00:11:32 +0530

    Add files via upload

---
+++ CSE/DAY 8/COMPOSIT NUMBERS (1).cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int num, i, count = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num <= 1) {
+        printf("Invalid input. Enter a number greater than 1.\n");
+        return 0;
+    }
+
+    for (i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            count++;
+            break;
+        }
+    }
+
+    if (count > 0)
+        printf("%d is a composite number.\n", num);
+    else
+        printf("%d is not a composite number.\n", num);
+
+    return 0;
+}
